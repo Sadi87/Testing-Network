06303c2253e88e8ba33078be2c9363061cfdc422
Sadi87 <s.m.musharaf87@gmail.com>
2022-03-24 10:11:59 +0000

    this is correct code for telnet

--- telnet1.py
+++ telnet1.py
@@ -1,19 +1,22 @@
 import getpass
 import telnetlib
 
-HOST = "localhost"
-user = input("Enter your remote account: ")
+HOST = "192.168.211.142"
+user = input("Enter username ")
 password = getpass.getpass()
 
 tn = telnetlib.Telnet(HOST)
 
-tn.read_until(b"login: ")
+tn.read_until(b"Username: ")
 tn.write(user.encode('ascii') + b"\n")
 if password:
     tn.read_until(b"Password: ")
     tn.write(password.encode('ascii') + b"\n")
 
-tn.write(b"ls\n")
-tn.write(b"exit\n")
+tn.write(b"en\n")
+tn.write(b"cisco\n")
+tn.write(b"conf t\n")
+tn.write(b"interface loop 1\n")
+
 
 print(tn.read_all().decode('ascii'))
