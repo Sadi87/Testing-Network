d3734c3bf631d2c9977ad0aa314324f9d90844af
Sadi87 <s.m.musharaf87@gmail.com>
2022-03-29 11:44:03 +0100

    this is not working

--- telnet1.py
+++ telnet1.py
@@ -1,8 +1,8 @@
 import getpass
 import telnetlib
 
-HOST = "192.168.211.142"
-user = input("Enter username ")
+HOST = "192.168.122.136"
+user = "admin"
 password = getpass.getpass()
 
 tn = telnetlib.Telnet(HOST)
@@ -17,6 +17,11 @@ tn.write(b"en\n")
 tn.write(b"cisco\n")
 tn.write(b"conf t\n")
 tn.write(b"interface loop 1\n")
-
-
+tn.write(b"ip add 10.10.10.1 255.255.255.0\n")
+tn.write(b"no shutdown\n")
+tn.write(b"interface loop 2\n")
+tn.write(b"ip add 11.10.10.1 255.255.255.0\n")
+tn.write(b"no shutdown\n")
+tn.write(b"exit\n")
+tn.write(b"end\n")
 print(tn.read_all().decode('ascii'))
