9e32f34465f38e63bef6cb41db7ce317ead1da77
Sadi87 <s.m.musharaf87@gmail.com>
2022-04-24 04:28:16 +0100

    this is new

--- vlan1.py
+++ vlan1.py
@@ -1,32 +1,12 @@
 from netmiko import ConnectHandler
 
-iosv_l2_s1 = {
-    'device_type': 'cisco_ios',
-    'ip': '192.168.122.72',
-    'username': 'david',
-    'password': 'cisco'
-}
-
-iosv_l2_s2 = {
-    'device_type': 'cisco_ios',
-    'ip': '192.168.122.82',
-    'username': 'david',
-    'password': 'cisco'
-}
-
-iosv_l2_s3 = {
-    'device_type': 'cisco_ios',
-    'ip': '192.168.122.83',
-    'username': 'david',
-    'password': 'cisco'
-}
-
-all_devices = [iosv_l2_s1, iosv_l2_s2, iosv_l2_s3]
-
-for devices in all_devices:
-    net_connect = ConnectHandler(**devices)
-    for n in range (2,21):
-       print ("Creating VLAN " + str(n))
-       config_commands = ['vlan ' + str(n), 'name Python_VLAN ' + str(n)]
-       output = net_connect.send_config_set(config_commands)
-       print (output)
+network_device = {
+        "device_type": "cisco_ios",
+        "host": "192.168.211.159",
+        "username": "admin",
+        "password" : "cisco",
+        }
+Connect = ConnectHandler(**network_device)
+Connect.enable()
+problm = "show ip int br"
+print(Connect.send_command(problm))
