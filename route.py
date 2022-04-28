aaa7a53f648ceea5df4da50cebd3f8d8b4c2cde1
Sadi87 <s.m.musharaf87@gmail.com>
2022-04-28 03:12:47 +0100

    this is last modify code for router

--- route.py
+++ route.py
@@ -28,17 +28,20 @@ for line in ip_list:
 	    
     for i in range (1,20):
         count = count + 1
-        ##j = i-1
-        ##ip = '192.168.211.1'
+        
         if HOST == '192.168.211.144':
            
-           
+           ## interface g0/1 configuration 
             tn.write(b"int g0/1\n")
             tn.write(b"ip address 172.16." + str(count + 1).encode('ascii') + b".2 255.255.255.0\n")
             tn.write(b"description to S1\n")
+            tn.write(b"standby 10 ip 172.16." + str(count + 1).encode('ascii') + b".1\n")
+            tn.write(b"standby 10 priority 255\n")
+            tn.write(b"standby 10 preempt\n")
             tn.write(b"no shutdown\n")
             tn.write(b"exit\n")
-
+            
+            ## Dhcp for int g0/1
             tn.write(b"ip dhcp exclude 172.16." + str(count + 1).encode('ascii') + b".1 172.16." + str(count + 1).encode('ascii') + b".15\n")
             tn.write(b"ip dhcp pool VLAN" + str(i).encode('ascii') + b"\n")
             tn.write(b"network 172.16." + str(count + 1).encode('ascii') + b".0 255.255.255.0\n")
@@ -46,12 +49,17 @@ for line in ip_list:
             tn.write(b"dns 8.8.8.8\n")
             tn.write(b"exit\n")
 
+            ## interface g0/2 config
             tn.write(b"int g0/2\n")
             tn.write(b"ip address 172.16." + str(count + 2).encode('ascii') + b".2 255.255.255.0\n")
+            tn.write(b"standby 10 ip 172.16." + str(count + 2).encode('ascii') + b".1\n")
+            tn.write(b"standby 10 priority 200\n")
+            tn.write(b"standby 10 preempt\n")
             tn.write(b"description to S2\n")
             tn.write(b"no shutdown\n")
             tn.write(b"exit\n")
 
+            ## Dhcp for int g0/2
             tn.write(b"ip dhcp exclude 172.16." + str(count + 2).encode('ascii') + b".1 172.16." + str(count + 2).encode('ascii') + b".15\n")
             tn.write(b"ip dhcp pool VLAN" + str(i).encode('ascii') + b"\n")
             tn.write(b"network 172.16." + str(count + 2).encode('ascii') + b".0 255.255.255.0\n")
@@ -59,25 +67,29 @@ for line in ip_list:
             tn.write(b"dns 8.8.8.8\n")
             tn.write(b"exit\n")
 
+            ## Routing Protocol config
             tn.write(b"router ospf 10\n")
             tn.write(b"router-id " + str(i).encode('ascii') + b"." + str(i).encode('ascii') + b"." + str(count).encode('ascii') + b"." + b"1 \n")
             tn.write(b"network 172.16." + str(count + 1).encode('ascii') + b".0 0.0.0.255 area 0 \n")
             tn.write(b"network 172.16." + str(count + 2).encode('ascii') + b".0 0.0.0.255 area 0 \n")
             
-
             break
 
             ## this is for another router 
         
         if HOST == '192.168.211.159':
             
-            
+            ## R2 int g0/1 config
             tn.write(b"int g0/1\n")
             tn.write(b"ip address 172.16." + str(count + 2).encode('ascii') + b".2 255.255.255.0\n")
             tn.write(b"description to S1\n")
+            tn.write(b"standby 10 ip 172.16." + str(count + 2).encode('ascii') + b".1\n")
+            tn.write(b"standby 10 priority 255\n")
+            tn.write(b"standby 10 preempt\n")
             tn.write(b"no shutdown\n")
             tn.write(b"exit\n")
 
+            ## R2  DHCP FOR int g0/1 config
             tn.write(b"ip dhcp exclude 172.16." + str(count + 2).encode('ascii') + b".1 172.16." + str(count + 2).encode('ascii') + b".15\n")
             tn.write(b"ip dhcp pool VLAN" + str(i).encode('ascii') + b"\n")
             tn.write(b"network 172.16." + str(count + 2).encode('ascii') + b".0 255.255.255.0\n")
@@ -85,12 +97,17 @@ for line in ip_list:
             tn.write(b"dns 8.8.8.8\n")
             tn.write(b"exit\n")
 
+            ## R2 int g0/2 config
             tn.write(b"int g0/2\n")
             tn.write(b"ip address 172.16." + str(count + 3).encode('ascii') + b".2 255.255.255.0\n")
             tn.write(b"description to S2\n")
+            tn.write(b"standby 10 ip 172.16." + str(count + 3).encode('ascii') + b".1\n")
+            tn.write(b"standby 10 priority 200\n")
+            tn.write(b"standby 10 preempt\n")
             tn.write(b"no shutdown\n")
             tn.write(b"exit\n")
 
+            ## R2 for int g0/1 config
             tn.write(b"ip dhcp exclude 172.16." + str(count + 3).encode('ascii') + b".1 172.16." + str(count + 3).encode('ascii') + b".15\n")
             tn.write(b"ip dhcp pool VLAN" + str(i).encode('ascii') + b"\n")
             tn.write(b"network 172.16." + str(count + 3).encode('ascii') + b".0 255.255.255.0\n")
@@ -98,6 +115,7 @@ for line in ip_list:
             tn.write(b"dns 8.8.8.8\n")
             tn.write(b"exit\n")
 
+            ## R2 Routing protocol config
             tn.write(b"router ospf 10\n")
             tn.write(b"router-id " + str(i).encode('ascii') + b"." + str(i).encode('ascii') + b"." + str(count).encode('ascii') + b"." + b"1 \n")
             tn.write(b"network 172.16." + str(count + 2).encode('ascii') + b".0 0.0.0.255 area 0 \n")
@@ -105,42 +123,7 @@ for line in ip_list:
                 
             break
 
-    if HOST == '192.168.211.144':
-            
-        tn.write(b"int g0/1\n")
-        tn.write(b"standby 10 ip 172.16." + str(count + 1).encode('ascii') + b".2 255.255.255.0\n")
-        tn.write(b"standby 10 priority 255\n")
-        tn.write(b"standby 10 preempt\n")
-            ##tn.write(b"standby 10 track interface g0/0\n")
-        tn.write(b"exit\n")
-
-
-        tn.write(b"int g0/2\n")
-        tn.write(b"ip address 172.16." + str(count + 2).encode('ascii') + b".1 255.255.255.0\n")
-        tn.write(b"standby 10 priority 200\n")
-        tn.write(b"standby 10 preempt\n")
-            ##tn.write(b"standby 10 track interface g0/0\n")
-        tn.write(b"exit\n") 
-        break
-
-    if HOST == '192.168.211.159':
-
-        tn.write(b"int g0/1\n")
-        tn.write(b"ip address 172.16." + str(count + 2).encode('ascii') + b".2 255.255.255.0\n")
-        tn.write(b"standby 10 priority 255\n")
-        tn.write(b"standby 10 preempt\n")
-            ##tn.write(b"standby 10 track interface g0/0\n")
-        tn.write(b"exit\n")
-
-
-        tn.write(b"int g0/2\n")
-        tn.write(b"ip address 172.16." + str(count + 3).encode('ascii') + b".1 255.255.255.0\n")
-        tn.write(b"standby 10 priority 20\n")
-        tn.write(b"standby 10 preempt\n")
-            ##tn.write(b"standby 10 track interface g0/0\n")
-        tn.write(b"exit\n") 
-        break
-    
+        
     tn.write(b"end\n")
     tn.write(b"wr\n")
     tn.write(b"exit\n")
