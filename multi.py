383eaa6d370f3c95ce788b33cb8ed5db0cad68c0
Sadi87 <s.m.musharaf87@gmail.com>
2022-05-05 16:16:08 +0100

    this i for building code

--- multi.py
+++ multi.py
@@ -1,15 +1,15 @@
-import imp
-from ipaddress import ip_address
+
+from ast import Index
 import telnetlib
 import getpass
 
+user = input("Enter the Telnet Username: ")
+password = getpass.getpass("Enter the telnet password: ")
 
-user = input("Enter the Username: ")
-password = getpass.getpass("Enter the Telnet Password: ")
-
-f_handle = open('ip.txt')
-
-for line in f_handle:
+with open('ip.txt') as f:
+    ip_list = f.read().splitlines()
+count = 2
+for line in ip_list:
     HOST = line.strip('\n')
     print("Telnet to host: " + HOST)
     tn = telnetlib.Telnet(HOST)
@@ -23,32 +23,59 @@ for line in f_handle:
     tn.write(b"enable\n")
     tn.write(b"cisco\n")
     tn.write(b"configure terminal\n")
-
-    for i in range(2, 10):
-        j = str(i)
-        tn.write(b"vlan " + j.encode('ascii') + b"\n")
-        tn.write(b"name VLAN_" + j.encode('ascii') + b"\n")
-        if i == 3:
-            tn.write(b"name Management" + b"\n")
-            tn.write(b"int vlan 3\n")
-            tn.write(b"ip address 192.168." +j.encode('ascii') + b"1.1 255.255.255.0\n")
-            tn.write(b"no shutdown\n")
-            tn.write(b"exit\n")
-        if i == 4:
-            tn.write(b"name Student" + b"\n")
-            tn.write(b"int vlan 4\n")
-            tn.write(b"ip address 192.168." +j.encode('ascii') + b"1.1 255.255.255.0\n")
-            tn.write(b"no shutdown\n")
-            tn.write(b"exit\n")
-        if i == 5:
-            tn.write(b"name Staff" + b"\n")
-            tn.write(b"int vlan 5\n")
-            tn.write(b"ip address 192.168." +j.encode('ascii') + b"1.1 255.255.255.0\n")
-            tn.write(b"no shutdown\n")
+    tn.write(b"no ip domain-lookup\n")
+    tn.write(b"banner motd #This is Network Automation Assesment. Done By Sadi#\n")
+	    
+    for i in range (1,20):
+        count = count + 1
         
-    
+        if Index(line) == HOST:
+           
+           ## interface g0/1 configuration 
+           ## tn.write(b"int g0/0\n")
+           ## tn.write(b"ip address dhcp\n")
+           ## tn.write(b"description to R1\n")
+           ## tn.write(b"no shutdown\n")
+           ## tn.write(b"exit\n")
 
-    
+            ##int g0/1 to switch 
+            tn.write(b"int g0/1\n")
+            tn.write(b"ip address 192.168." + str(i).encode('ascii') + b"." + str(count + 1).encode('ascii') + b" 255.255.255.0\n")
+            tn.write(b"standby 10 ip 192.168." + str(i).encode('ascii') + b".1\n")
+            tn.write(b"standby 10 priority " + str(count + 50).encode('ascii') + b"\n")
+            tn.write(b"standby 10 preempt\n")
+            tn.write(b"no shutdown\n")
+            tn.write(b"exit\n")
+            tn.write(b"ip default-gateway 192.168." + str(i).encode('ascii') + b".1\n")
+
+            
+
+            ## Dhcp for Interface For G0/1
+            tn.write(b"ip dhcp exclude 192.168." + str(i).encode('ascii') + b".1 192.168." + str(i).encode('ascii') + b".10\n")
+            tn.write(b"ip dhcp pool VLAN_" + str(count + 2).encode('ascii') + b"\n")
+            tn.write(b"network 192.168." + str(i).encode('ascii') + b".0 255.255.255.0\n")
+            tn.write(b"defau 192.168." + str(i).encode('ascii') + b".1\n")
+            tn.write(b"dns 8.8.8.8\n")
+            tn.write(b"exit\n")
+
+           
+
+
+            ## Routing Protocol config
+            tn.write(b"router ospf 10\n")
+            tn.write(b"router-id " + str(i).encode('ascii') + b"." + str(i).encode('ascii') + b"." + str(count + 5).encode('ascii') + b"." + b"1 \n")
+            tn.write(b"network 192.168." + str(i).encode('ascii') + b".0 0.0.0.255 area 0 \n")
+           ## tn.write(b"network 192.168." + str(count + 2).encode('ascii') + b".0 0.0.0.255 area 0 \n")
+            tn.write(b"network 0.0.0.0 0.0.0.0 area 0\n")
+            tn.write(b"network " + str(HOST).encode('ascii') + b" 0.0.0.255 area 0\n")
+            
+            break
+ 
+       
+
+        
     tn.write(b"end\n")
+    ##tn.write(b"wr\n")
+    tn.write(b"show ip int br\n")
     tn.write(b"exit\n")
     print(tn.read_all().decode())
