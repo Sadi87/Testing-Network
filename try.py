93900e67b50947b13e043bc63ca78404d20fafdc
Sadi87 <s.m.musharaf87@gmail.com>
2022-06-27 14:26:07 +0100

    okay all are work properly

--- try.py
+++ try.py
@@ -1,15 +1,18 @@
 from netmiko import ConnectHandler
 
-cisco_iso = {
-    "device name": "cisco_iso",
-    "ip": "192.168.211.144",
+
+r1 = {
+    "device_type": "cisco_ios",
+    "ip":"192.168.211.170",
     "username": "admin",
     "password": "cisco",
-    "secret": "cisco"
-}
-net_connect = ConnectHandler(**cisco_iso) 
+    "secret": "cisco",
+     }
+     
+net_connect = ConnectHandler(**r1)
 net_connect.enable()
 
-output = net_connect.send_command("show ip int br")
-print(output)
+result = net_connect.send_command('show ip int br')
+
+print(result)
 
